dd4a82f06a7b3d6d069e9834927c02adc337a5b3
NiightGamez <31709198+NiightGamez@users.noreply.github.com>
2019-01-23 19:07:31 -0500

    Create Matrix.bat

---
+++ Fun/Matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+color 2
+title Matrix
+:start
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto start
